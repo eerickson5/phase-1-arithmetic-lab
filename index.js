0c5578622449b99163a58c7d48b3942b823964bb
Elise Erickson <elisece717@gmail.com>
2023-11-20 15:57:46 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 12) + 1;
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(2, 10, 18, 20);
